07b136eeccae84b2a6f67a5d5f2bae517b45cf03
Numi2 <33553832+Numi2@users.noreply.github.com>
2023-12-28 01:12:07 +0100

    4

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -10,7 +10,7 @@ export function Homepage() {
     <>
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
-          <CrossIcon className="h-6 w-6" />
+         
           <span className="ml-2 text-lg font-semibold">eMedic</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
@@ -47,22 +47,3 @@ export function Homepage() {
   )
 }
 
-
-function CrossIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M11 2a2 2 0 0 0-2 2v5H4a2 2 0 0 0-2 2v2c0 1.1.9 2 2 2h5v5c0 1.1.9 2 2 2h2a2 2 0 0 0 2-2v-5h5a2 2 0 0 0 2-2v-2a2 2 0 0 0-2-2h-5V4a2 2 0 0 0-2-2h-2z" />
-    </svg>
-  )
-}
